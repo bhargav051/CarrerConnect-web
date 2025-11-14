3d4d2337b4a39631127a9e7ce2737cabadd0ef3b
Bhargav Jitendra Kasundra <bhargav111.kasundra@gmail.com>
2025-11-14 12:08:57 +0530

    dynamic base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-export const BASE_URL = "http://localhost:3000";
+let BASE_URL = "";
+
+if (import.meta.env.MODE === "development") {
+  // When running locally → Vite dev mode
+  BASE_URL = "http://localhost:3000";
+} else {
+  // When deployed → Vercel production build
+  BASE_URL = "https://carrerconnect-kvrj.onrender.com";
+}
+
+export { BASE_URL };
